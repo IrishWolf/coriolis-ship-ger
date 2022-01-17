aa068de874fbff6e172958e6184497b35d2594dd
IrishWolf <tyr05@hotmail.de>
2022-01-17 23:17:13 +0100

    translation of features (first part)

--- src/app/features.ts
+++ src/app/features.ts
@@ -1,56 +1,16 @@
 export default [
   {
     id: 1,
-    name: 'Ablative Armor',
-    benefit: 'Reduces damage by 3 for one attack only',
+    name: 'Ablative Panzerung',
+    benefit: 'Verringert Schaden für einen Angriff um 3',
     cost: 10,
     bonusses: [],
     selected: false
   },
   {
     id: 2,
-    name: 'Advanced Countermeasures',
-    benefit: 'Gives +1 when launching countermeasures',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 3,
-    name: 'Advanced Targeting Computer',
-    benefit: 'Gives +1 when locking onto targets.',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 4,
-    name: 'Advanced Torpedoes',
-    benefit: 'Gives -1 to the target ships countermeasures',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 5,
-    name: 'Advance Weapon System',
-    benefit: 'Gives +1 when firing a specific weapon system.',
-    cost: 20,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 6,
-    name: 'Advanced Workshop',
-    benefit: 'Can repair Advanced technology.',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 7,
-    name: 'Antimatter Rockets',
-    benefit: 'Maneuverability +2',
+    name: 'Antimaterieantrieb',
+    benefit: 'Manövrierfähigkeit +2',
     cost: 20,
     bonusses: [
       {
@@ -61,65 +21,181 @@ export default [
     selected: false
   },
   {
-    id: 8,
+    id: 3,
     name: 'Arboretum',
-    benefit: 'Helps the crew recover Mind Points',
+    benefit: 'Hilft der Besatzung dabei, Willenskraftpunkte aufzufrischen',
+    cost: 5,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 4,
+    name: 'Atmosphäreneintritt',
+    benefit: 'Kann auf Planeten landen/abheben.',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 5,
+    name: 'Aufgebohrter Beschleuniger',
+    benefit: 'Verleiht +2 auf die Aktion Vorrücken/Rückzug',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 6,
+    name: 'Auswurfsystem',
+    benefit: 'Rettungskapsel auf der Brücke',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 7,
+    name: 'Bergungseinheit',
+    benefit: 'Ermöglicht Bergungen',
+    cost: 10,
+    selected: false
+  },
+  {
+    id: 8,
+    name: 'Bibliotheksdatenbank',
+    benefit: 'Verleiht +3 auf Würfe mit Kultur oder Wissenschaft',
     cost: 5,
     bonusses: [],
     selected: false
   },
   {
     id: 9,
-    name: 'Atmospheric Entry',
-    benefit: 'Land on/take off from planets.',
-    cost: 10,
+    name: 'ED-Felder',
+    benefit: 'Schützt vor explosiver Dekompression',
+    cost: 20,
     bonusses: [],
     selected: false
   },
   {
     id: 10,
-    name: 'Blessed Ship',
-    benefit: 'Bonus +1 to portal jumps.',
-    cost: 5,
+    name: 'Empfindliche Sensoren',
+    benefit: 'Verleiht +1 auf Sensorwürfe',
+    cost: 10,
     bonusses: [],
     selected: false
   },
   {
     id: 11,
-    name: 'Bonus Modules',
-    benefit: 'Increassed capacity for carrying extra modules.',
-    cost: 20,
+    name: 'Externe Fracht',
+    benefit: 'Fracht wird im Vakuum außen am Rumpf befestigt',
+    cost: 10,
     bonusses: [],
     selected: false
   },
   {
     id: 12,
-    name: 'ED Fields',
-    benefit: 'Protects against explosive decompression.',
-    cost: 20,
+    name: 'Forschungscomputer',
+    benefit: 'Verleiht +1 auf Wissenschaft für Analysen',
+    cost: 5,
     bonusses: [],
     selected: false
   },
   {
     id: 13,
-    name: 'Ejector',
-    benefit: 'Escape pod on the bridge',
+    name: 'Fortschrittlicher Feuerleitcomputer',
+    benefit: 'Verleiht +1 zur Zielerfassung.',
     cost: 10,
     bonusses: [],
     selected: false
   },
   {
     id: 14,
-    name: 'External Cargo',
-    benefit: 'Cargo on the outside of the hull, in vacuum.',
+    name: 'Fortschrittliche Täuschkörper',
+    benefit: 'Verleiht +1 beim Einsatz von Gegenmaßnahmen',
     cost: 10,
     bonusses: [],
     selected: false
   },
   {
     id: 15,
-    name: 'Heavy Armor',
-    benefit: 'Armor Rating +1, Maneuverability -1',
+    name: 'Fortschrittliche Torpedos',
+    benefit: 'Verleiht -1 auf die Gegenmaßnahmen des Zielschiffs',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 16,
+    name: 'Fortschrittliche Waffensysteme',
+    benefit: 'Verleiht +1 beim Abfeuern eines bestimmten Waffensystems',
+    cost: 20,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 17,
+    name: 'Fortschrittliche Werkstatt',
+    benefit: 'Kann fortschrittliche Technologie reparieren',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 18,
+    name: 'Gesegnetes Schiff',
+    benefit: 'Bonus von +1 auf Portalsprünge',
+    cost: 5,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 19,
+    name: 'Präzise Schubdüsen',
+    benefit: 'Bonus von +2 auf Pilot, wenn du andocken oder landen willst',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 20,
+    name: 'Reaktorschub',
+    benefit: 'Verleiht +3 LP beim Überladen des Reaktors',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 21,
+    name: 'Robuster Rumpf',
+    benefit: 'Verleiht +1 Rumpfpunkt',
+    cost: 10,
+    bonusses: [
+      {
+        spec: 'hp',
+        bonus: 1
+      }
+    ],
+    selected: false
+  },
+  {
+    id: 22,
+    name: 'Schiffsintelligenz',
+    benefit: 'Vielseitige KI, alle Attributswerte 1, relevante Fertigkeitsstufen 3',
+    cost: 30,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 23,
+    name: 'Schiffssystem',
+    benefit: 'Ersetzt ein Besatzungsmitglied, Attribut 3, Fertigkeitsstufe 3',
+    cost: 20,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 24,
+    name: 'Schwere Panzerung',
+    benefit: 'Panzerungswert +1, Manövrierfähigkeit -1',
     cost: 10,
     bonusses: [
       {
@@ -134,86 +210,17 @@ export default [
     selected: false
   },
   {
-    id: 16,
-    name: 'Library Database',
-    benefit: 'Gives you +3 to CULTURE or SCIENCE rolls',
-    cost: 5,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 17,
-    name: 'Precise Thrusters',
-    benefit: 'Bonus +2 to PILOT when docking or landing',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 18,
-    name: 'Reactor Burst',
-    benefit: 'Gives +3 EP when overloading reactor',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 19,
-    name: 'Research Computer',
-    benefit: 'Gives +1 to SCIENCE for analyses',
-    cost: 5,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 20,
-    name: 'Robust Hull',
-    benefit: 'Gives +1 Hull Point.',
-    cost: 10,
-    bonusses: [
-      {
-        spec: 'hp',
-        bonus: 1
-      }
-    ],
-    selected: false
-  },
-  {
-    id: 21,
-    name: 'Salvage Unit',
-    benefit: 'Enables salvaging.',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 22,
-    name: 'Sensitive Sensors',
-    benefit: 'Gives +1 to all sensor rolls',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 23,
-    name: 'Ship Intelligence',
-    benefit: 'Versatile AI, all attribute scores 1, relevant skill levels 3.',
-    cost: 30,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 24,
-    name: 'Ship System',
-    benefit: 'Replace one crew member, attribute 3, skill level 3.',
+    id: 25,
+    name: 'Supersensoren',
+    benefit: 'Erhöht die Reichweite der Schiffssensoren auf Extrem',
     cost: 20,
     bonusses: [],
     selected: false
   },
   {
-    id: 25,
-    name: 'Stealthe Technology',
-    benefit: 'Decreases ship Signature by 1',
+    id: 26,
+    name: 'Tarntechnologie',
+    benefit: 'Verringert die Schiffssignatur um 1',
     cost: 20,
     bonusses: [
       {
@@ -224,46 +231,17 @@ export default [
     selected: false
   },
   {
-    id: 26,
-    name: 'Supercharged reactor',
-    benefit: 'Gives the ship +1 Engery Point.',
-    cost: 10,
-    bonusses: [
-      {
-        spec: 'ep',
-        bonus: 1
-      }
-    ],
-    selected: false
-  },
-  {
     id: 27,
-    name: 'Super Sensors',
-    benefit: 'Increases range of ship sensors to Extreme.',
-    cost: 20,
+    name: 'Traumalabor',
+    benefit: 'Verleiht einen Bonus auf Medikurgie-Würfe',
+    cost: 10,
     bonusses: [],
     selected: false
   },
   {
     id: 28,
-    name: 'Trauma Lab',
-    benefit: 'Gives bonus to MEDICURGY rolls.',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 29,
-    name: 'Tuned Accelerator',
-    benefit: 'Bonus +2 to the advance/retreat action.',
-    cost: 10,
-    bonusses: [],
-    selected: false
-  },
-  {
-    id: 30,
-    name: 'Turbo Projector',
-    benefit: 'Gives +1 to Maneuverability and Speed',
+    name: 'Turboprojektor',
+    benefit: 'Verleiht +1 Manövrierfähigkeit und Geschwindigkeit',
     cost: 20,
     bonusses: [
       {
@@ -272,6 +250,27 @@ export default [
       },
       {
         spec: 'speed',
+        bonus: 1
+      }
+    ],
+    selected: false
+  },
+  {
+    id: 29,
+    name: 'Überladener Reaktor',
+    benefit: 'Verleiht dem Schiff +1 Leistungspunkt',
+    cost: 10,
+    bonusses: [],
+    selected: false
+  },
+  {
+    id: 30,
+    name: 'Zusätzliche Module',
+    benefit: 'Zusätzlicher Raum für weitere Module',
+    cost: 20,
+    bonusses: [
+      {
+        spec: 'ep',
         bonus: 1
       }
     ],
